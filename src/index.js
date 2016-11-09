0dbb6617d9554793cc4c9c1cd44da8c5067f05df
y.j.zhang <y.j.zhang@medilink.com.cn>
2016-11-09 10:00:19 +0800

    summary

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
+/*
+* Adding React to an Existing Application
+* npm install --save react react-dom
+*
+*
+* Creating a Single Page Application
+* npm install -g create-react-app
+* create-react-app hello-world
+* cd hello-world
+* npm start
+*
+*
+* Using a CDN
+* <script src="https://unpkg.com/react@15/dist/react.js"></script>
+* <script src="https://unpkg.com/react-dom@15/dist/react-dom.js"></script>
+*
+* <script src="https://unpkg.com/react@15/dist/react.min.js"></script>
+* <script src="https://unpkg.com/react-dom@15/dist/react-dom.min.js"></script>
+* */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
